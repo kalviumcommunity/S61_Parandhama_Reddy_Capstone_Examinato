db65c5bff17da599c4206ba7b9cce5b1be698b12
BOMMAKAPARANDHAMAREDDY <149357107+BOMMAKAPARANDHAMAREDDY@users.noreply.github.com>
2024-06-05 09:38:58 +0530

    Added File upload Functionality

--- Backend/Model/loginSchema.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-const loginSchema = new Schema({
-    email:String,
-    password:String
-})
--- Client/src/Components/QuizPreview.jsx
+++ Client/src/Components/QuizPreview.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
 
-const QuizPreview = ({ questions }) => {
+const QuizPreview = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const { quizData } = location.state || { quizData: { questions: [] } };
+  const { questions } = quizData;
 
   const handleHomeClick = () => {
     navigate("/home");
@@ -13,20 +17,46 @@ const QuizPreview = ({ questions }) => {
   return (
     <div>
       <div className="p-2 bg-white drop-shadow-lg sticky top-0 rounded-3xl">
-        <button onClick={handleHomeClick}>Back To Home</button>
+        <FaArrowLeft className="h-8 w-20 cursor-pointer" onClick={handleHomeClick} />
       </div>
-      <div className="p-20 bg-[#E9EDC9]">
+      <div className="p-10 bg-[#E9EDC9]">
         <div className="bg-[#E9EDC9] flex gap-5 box-border">
           <div className="flex-col justify-center items-center text-center w-full gap-5 p-5 bg-white border h-auto overflow-hidden shadow-lg rounded">
             {questions.map((q, index) => (
-              <div key={index} className="border-[1px] p-10 mb-4">
-                <div className="mb-5 font-bold">{q.question}</div>
+              <div
+                key={index}
+                className="flex-col justify-center border-[1px] p-10"
+              >
+                <div className="mb-5 font-bold">
+                  {typeof q.question === "string" &&
+                  q.question.startsWith("data:image/") ? (
+                    <img
+                      src={q.question}
+                      alt={`Question ${index + 1}`}
+                      className="h-[25vh] ml-96"
+                    />
+                  ) : (
+                    q.question
+                  )}
+                </div>
                 <div className="grid grid-cols-2 gap-2">
                   {q.options.map((opt, optIndex) => (
-                    <div key={optIndex}>
-                      <button className="bg-black text-white w-[50%] p-4 rounded-2xl">
-                        {opt}
-                      </button>
+                    <div
+                      key={optIndex}
+                      className="flex justify-center items-center"
+                    >
+                      {typeof opt === "string" &&
+                      opt.startsWith("data:image/") ? (
+                        <img
+                          src={opt}
+                          alt={`Option ${optIndex + 1}`}
+                          className="h-[20vh]"
+                        />
+                      ) : (
+                        <button className="bg-black text-white w-[50%] p-4 rounded-2xl">
+                          {opt}
+                        </button>
+                      )}
                     </div>
                   ))}
                 </div>
